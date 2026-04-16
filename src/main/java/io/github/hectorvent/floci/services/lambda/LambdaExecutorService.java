dff4bea28d4f09e122dadde5a67f1835385b6fdc
Ricardo Lima <ricardo.hrlima@gmail.com>
2026-04-16 15:13:39 -0300

    fix(lambda): release container handle on timeout instead of draining warm pool

--- src/main/java/io/github/hectorvent/floci/services/lambda/LambdaExecutorService.java
+++ src/main/java/io/github/hectorvent/floci/services/lambda/LambdaExecutorService.java
@@ -107,7 +107,7 @@ public class LambdaExecutorService {
 
         } catch (TimeoutException e) {
             LOG.warnv("Function {0} timed out after {1}s", fn.getFunctionName(), fn.getTimeout());
-            warmPool.drainFunction(fn.getFunctionName());
+            warmPool.release(handle);
             return new InvokeResult(200, "Unhandled",
                     buildErrorPayload("Task timed out after " + fn.getTimeout() + " seconds", "Function.TimedOut"),
                     null, requestId);
