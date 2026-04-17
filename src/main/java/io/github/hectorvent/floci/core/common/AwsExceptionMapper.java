77b60e135d072f8bbf33c1f422c0069b3df0237a
Bria Kindersley <bria.kindersley@trulioo.com>
2026-04-17 13:34:06 -0700

    integration test

--- src/main/java/io/github/hectorvent/floci/core/common/AwsExceptionMapper.java
+++ src/main/java/io/github/hectorvent/floci/core/common/AwsExceptionMapper.java
@@ -1,6 +1,5 @@
 package io.github.hectorvent.floci.core.common;
 
-import io.github.hectorvent.floci.services.dynamodb.model.ConditionalCheckFailedException;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
@@ -18,12 +17,6 @@ public class AwsExceptionMapper implements ExceptionMapper<AwsException> {
     @Override
     public Response toResponse(AwsException exception) {
         LOG.debugv("Mapping exception: {0} - {1}", exception.getErrorCode(), exception.getMessage());
-        if (exception instanceof ConditionalCheckFailedException){
-            return Response.status(exception.getHttpStatus())
-                .type(MediaType.APPLICATION_JSON)
-                .entity(new AwsErrorResponseWithItem(exception.jsonType(), exception.getMessage(), ((ConditionalCheckFailedException) exception).getItem()))
-                .build();
-        }
         return Response.status(exception.getHttpStatus())
                 .type(MediaType.APPLICATION_JSON)
                 .entity(new AwsErrorResponse(exception.jsonType(), exception.getMessage()))
