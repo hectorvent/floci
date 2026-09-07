653e0eb5d86591455c9bc8c6a9ceca95872bdaf6
Vu The Huy <vuthehuy.t61@gmail.com>
2026-09-07 19:18:56 +0700

    test(redshift-data): end-to-end Data API over a real cluster plus SDK compatibility test

--- compatibility-tests/sdk-test-java/src/main/java/com/floci/test/TestFixtures.java
+++ compatibility-tests/sdk-test-java/src/main/java/com/floci/test/TestFixtures.java
@@ -29,6 +29,7 @@ import software.amazon.awssdk.services.opensearch.OpenSearchClient;
 import software.amazon.awssdk.services.neptune.NeptuneClient;
 import software.amazon.awssdk.services.rds.RdsClient;
 import software.amazon.awssdk.services.redshift.RedshiftClient;
+import software.amazon.awssdk.services.redshiftdata.RedshiftDataClient;
 import software.amazon.awssdk.services.guardduty.GuardDutyClient;
 import software.amazon.awssdk.services.fis.FisClient;
 import software.amazon.awssdk.services.organizations.OrganizationsClient;
@@ -1109,6 +1110,14 @@ public final class TestFixtures {
 
     public static RedshiftClient redshiftClient() {
         return RedshiftClient.builder()
+                .endpointOverride(ENDPOINT)
+                .region(REGION)
+                .credentialsProvider(CREDENTIALS)
+                .build();
+    }
+
+    public static RedshiftDataClient redshiftDataClient() {
+        return RedshiftDataClient.builder()
                 .endpointOverride(ENDPOINT)
                 .region(REGION)
                 .credentialsProvider(CREDENTIALS)
